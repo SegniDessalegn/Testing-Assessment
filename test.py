86fead6a80dc80ce21e9822c485898f779126cd2
Segni Dessalegn <segnides@gmail.com>
2023-03-27 21:30:30 +0300

    add tests

--- test.py
+++ test.py
@@ -2,16 +2,21 @@ import unittest
 from string_functions import *
 
 class TestToUpper(unittest.TestCase):
-    # write your code here
-    pass
+    def test_upper(self):
+        self.assertEqual(to_upper('test text'), 'TEST TEXT')
+        self.assertEqual(to_upper('TEST TEXT'), 'TEST TEXT')
 
 class TestToLower(unittest.TestCase):
-    # Write you code here
-    pass
+    def test_lower(self):
+        self.assertEqual(to_lower('TEST TEXT'), 'test text')
+        self.assertEqual(to_lower('Test text'), 'test text')
 
 class TestCapitalize(unittest.TestCase):
-    # Write your code here
-    pass
+    def test_capitalize(self):
+        self.assertEqual(capitalize('test text'), 'Test text')
+        self.assertEqual(capitalize('Test text'), 'Test text')
+        self.assertEqual(capitalize('Test Text'), 'Test text')
+
 
 if __name__ == '__main__':
     unittest.main()
